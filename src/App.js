d333604950af75e6ed4adb07ba52607a5a427034
Chissypre <127617061+Chissypre@users.noreply.github.com>
2023-09-30 02:05:13 +0100

    removed asset svg style

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import BalanceSection from './BalanceSection/BalanceSection';
 import IncomeandExpense from './IncomeandExpense/IncomeandExpense'
 import AddTransaction from './AddTransaction/AddTransaction';
 import TransactionHistory from './TransactionHistory/TransactionHistory';
-import { ReactComponent as Subtle } from '../src/assets/subtle-prism.svg';
+
 
 import './App.css';
 
